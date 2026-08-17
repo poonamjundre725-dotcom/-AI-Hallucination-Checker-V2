0330bdde549f31df29a72e738d55a4c93b381e3f
Poonam Jundre <poonamjundre725@gmail.com>
2026-08-17 21:56:52 +0530

    Fix Groq model

--- llm.py
+++ llm.py
@@ -1,8 +1,5 @@
 import os
-from dotenv import load_dotenv
 from groq import Groq
-
-load_dotenv()
 
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY")
@@ -11,7 +8,7 @@ client = Groq(
 def ask_gemini(question):
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "user",
